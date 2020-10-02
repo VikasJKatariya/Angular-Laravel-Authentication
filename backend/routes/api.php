64836d63ca4f61f8583edcaaa985007bf3cc9a28
VikasJKatariya <vikaskatariya16081998@gmail.com>
2020-10-03 01:39:14 +0530

    JWT auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,20 @@
-<?php
+ <?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Route::group([
 
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +26,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
